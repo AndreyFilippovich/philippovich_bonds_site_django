fa3306e7f72b530c60f08c543833d45045146602
Андрей Филиппович <andrejfilippovic8@gmail.com>
2023-06-15 22:04:42 +0300

    add extra forms

--- philippovich_bonds/posts/models.py
+++ philippovich_bonds/posts/models.py
@@ -3,10 +3,10 @@ from django.urls import reverse
 
 
 class Posts(models.Model):
-    title = models.CharField(max_length=200, verbose_name = 'Заголовок')
+    title = models.CharField(max_length=255, verbose_name = 'Заголовок')
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
     content = models.TextField(blank=True, verbose_name = 'Текст статьи')
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name = 'Фото')
+    photo = models.ImageField(blank=True, upload_to="photos/%Y/%m/%d/", verbose_name = 'Фото')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name = 'Время создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name = 'Время изменения')
     is_published = models.BooleanField(default=True, verbose_name = 'Публикация')
